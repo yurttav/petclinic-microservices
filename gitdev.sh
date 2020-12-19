c3d3caa82f4f41e483db376effffcbbb548c0481
Baris Yurttav <ryurttav@yahoo.com>
2020-12-19 16:52:58 +0000

    $1

--- gitdev.sh
+++ gitdev.sh
@@ -1,6 +1,19 @@
-git add .
-git commit -m '$1'
-git push -u origin $2
-git checkout dev
-git merge $2
-git push origin dev
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    echo "This script need 2 parameters"
+    echo "First one should be in quotes '' which is for git m message"
+    echo "Second one is for branchname"
+elif [ "$#" -ne 2 ]
+then
+    echo "This script need 2 parameters"
+    echo "First one should be in quotes '' which is for git m message"
+    echo "Second one is for branchname"
+else 
+    git add .
+    git commit -m '$1'
+    git push -u origin $2
+    git checkout dev
+    git merge $2
+    git push origin dev
+fi
