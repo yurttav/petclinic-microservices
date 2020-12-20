e635b642393ffb03c6cd94364a81c22c3952fa0e
Baris Yurttav <ryurttav@yahoo.com>
2020-12-20 15:11:51 +0000

    updated gitdev.sh

--- gitdev.sh
+++ gitdev.sh
@@ -2,12 +2,13 @@ if [ $# -eq 0 ]
 then
     echo "No arguments supplied"
     echo "This script need 2 parameters"
-    echo "First one should be in quotes '' which is for git m message"
+    echo "First one should be in single quotes '' which is for git m message"
     echo "Second one is for branchname"
-elif [ "$#" -ne 2 ]
+elif [ $# -ne 2 ]
 then
+    echo "Invalid parameter count"
     echo "This script need 2 parameters"
-    echo "First one should be in quotes '' which is for git m message"
+    echo "First one should be in single quotes '' which is for git m message"
     echo "Second one is for branchname"
 else 
     git add .
