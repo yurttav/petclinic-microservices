266ac3c246ba1b65bbc123e799240ecd4f1dfeb6
Baris Yurttav <ryurttav@yahoo.com>
2020-12-26 17:26:02 +0000

    added qa automation pipeline for dev

--- selenium-jobs/test_veterinarians_headless.py
+++ selenium-jobs/test_veterinarians_headless.py
@@ -3,22 +3,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import os
 
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
+chrome_options.add_argument("no-sandbox")
+chrome_options.add_argument("disable-dev-shm-usage")
 
 # Update webdriver instance of chrome-driver with adding chrome options
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 # Connect to the application
-url = "http://ec2-34-236-170-96.compute-1.amazonaws.com:8080"
+APP_IP = os.environ['MASTER_PUBLIC_IP']
+url = "http://"+APP_IP.strip()+":8080/"
+print(url)
 driver.get(url)
+sleep(3)
 vet_link = driver.find_element_by_link_text("VETERINARIANS")
 vet_link.click()
 
 # Verify that table loaded
-sleep(1)
+sleep(5)
 verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
 
 print("Table loaded")
